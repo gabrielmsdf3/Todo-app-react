460751c261ace97e60780aeb3e537f6568e26462
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-03-17 17:31:56 -0300

    First commit

---
+++ frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+console.log(ReactDOM);
+export default ReactDOM.render(<App/>, document.getElementById('app'))
